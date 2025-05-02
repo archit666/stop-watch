6186b334faf33394c7301290cd45918ac229d74c
rifleking <rifleking1@gmail.com>
2025-05-02 17:00:32 +0530

    added actual stop watch

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -1,23 +1,27 @@
 import { useEffect, useRef, useState } from 'react';
 function StopWatch() {
 
-    const [seconds, setSeconds] = useState(0);
+    const [elapsedTime, setElapsedTime] = useState(0);
     const [running, setRunning] = useState(false);
 
-    const timeRef = useRef(null);
+    const intervalRef = useRef(null);
+    const currentTimeRef = useRef(0);
 
     useEffect(() => {
+        console.log('after clean up');
         if(running) {
-            timeRef.current = setInterval(() => {
-            setSeconds(prev => prev + 1);
-        }, 1000)
+            console.log('after clean up inside function');
+            intervalRef.current = setInterval(() => {
+            setElapsedTime(Date.now() - currentTimeRef.current);
+        }, 10)
     }
 
-        return () => { clearInterval(timeRef.current) };
+        return () => { console.log('clean up'); clearInterval(intervalRef.current) };
     }, [running]);
 
     const onStart = () => {
         setRunning(true);
+        currentTimeRef.current = Date.now() - elapsedTime;
     }
 
     const onPause = () => {
@@ -25,13 +29,23 @@ function StopWatch() {
     }
 
     const onReset = () => {
-        setSeconds(0);
+        setElapsedTime(0);
         setRunning(false);
+    }
+
+    const formatTime = () => {
+        // let hours = Math.floor(elapsedTime/(1000 * 60 * 60));
+        let min = Math.floor((elapsedTime/(1000 * 60)) % 60).toString().padStart(2, "0");
+        let sec = Math.floor(elapsedTime/(1000) % 60).toString().padStart(2, "0");
+        let milli = Math.floor((elapsedTime % 1000)/10).toString().padStart(2, "0");
+
+        return `${min} : ${sec} : ${milli}`;
+
     }
 
     return (
     <div style = {{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', border: '1px solid black', margin: '0 auto', width: '300px', borderRadius: '10px', overflow: 'hidden' }}>
-        <div style={{ background: 'black', color: 'white', width: '100%', textAlign: 'center', padding: '20px', fontSize: '18px' }}>{seconds}</div>
+        <div style={{ background: 'black', color: 'white', width: '100%', textAlign: 'center', padding: '20px', fontSize: '18px' }}>{formatTime()}</div>
         <div style = {{ display: 'flex', width: '100%', overflow: 'hidden', textAlign: 'center' }}>
             <div style = {{ flex: 1, borderRight: '2px solid black', cursor: 'pointer', padding: '16px 0px' }} onClick={onStart}>Start</div>
             <div style = {{ flex: 1, borderRight: '2px solid black', cursor: 'pointer', padding: '16px 0px' }} onClick={onPause}>Pause</div>
